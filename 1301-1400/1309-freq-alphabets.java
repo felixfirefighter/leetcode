bec589800d94042516c5e068435db7302a1f03ca
Yoong Ti Lee <leeyoongti@gmail.com>
2020-07-03 19:47:06 +0800

    add method 2 for 1309

--- 1301-1400/1309-freq-alphabets.java
+++ 1301-1400/1309-freq-alphabets.java
@@ -25,3 +25,25 @@ class Solution {
       return (char)((int)'a' + res - 1);
   }
 }
+
+// Method 2
+// O(n), 
+// 0ms, 100%
+// 37.1MB, 98.07%
+// https://leetcode.com/problems/decrypt-string-from-alphabet-to-integer-mapping/discuss/470686/Check-i-%2B-2
+class Solution {
+  public String freqAlphabets(String s) {
+      StringBuffer sb = new StringBuffer();
+      for(int i=0;i<s.length();i++) {
+          if(i < s.length() - 2 && s.charAt(i+2) == '#') {
+              int n = (s.charAt(i) - '1') * 10 + (s.charAt(i+1) - '0');
+              sb.append((char)('j' + n));
+              i += 2;
+          } else {
+              sb.append((char)('a' + s.charAt(i) - '1'));
+          }
+      }
+      
+      return sb.toString();
+  }
+}
