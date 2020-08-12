06b56aee50a50297aa18b7bf94d03e384e01471e
Lee Yoong Ti <leeyoongti@gmail.com>
2020-08-12 22:24:02 +0800

    add 119

---
+++ 101-200/119-get-row.java
@@ -0,0 +1,25 @@
+// O(n^2)
+// 1 ms, faster than 86.53%
+// 37.1 MB, less than 63.15% 
+class Solution {
+  public List<Integer> getRow(int rowIndex) {
+    var res = new ArrayList<Integer>();
+    res.add(1);
+    
+    for(var i=1;i<=rowIndex;i++) {
+      var prev = new ArrayList<Integer>(res);
+      
+      // add first element, always 1
+      res = new ArrayList<Integer>();
+      res.add(1);
+      for(var j=1;j<i;j++) {
+        res.add(prev.get(j-1) + prev.get(j));
+      }
+      
+      // add last element, always 1
+      res.add(1);
+    }
+    
+    return res;
+  }
+}
