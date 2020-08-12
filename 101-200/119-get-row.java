327b172d726e0354c699aed049fe5b04dea2c163
Lee Yoong Ti <leeyoongti@gmail.com>
2020-08-12 22:32:39 +0800

    add O(k) space solution

--- 101-200/119-get-row.java
+++ 101-200/119-get-row.java
@@ -23,3 +23,21 @@ class Solution {
     return res;
   }
 }
+
+// https://leetcode.com/problems/pascals-triangle-ii/
+class Solution {
+  public List<Integer> getRow(int rowIndex) {
+    var res = new ArrayList<Integer>();
+    res.add(1);
+    
+    for(var i=1;i<=rowIndex;i++) {
+      for(var j=i-1;j>=1;j--) {
+        var temp = res.get(j-1) + res.get(j);
+        res.set(j, temp);
+      }
+      res.add(1);
+    }
+    
+    return res;
+  }
+}
