782e12464ffbaebe1562cfb052f9492f38526970
Lee Yoong Ti <leeyoongti@gmail.com>
2020-08-24 20:49:06 +0800

    add 404

---
+++ 401-500/404-sum-of-left-leaves.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+  public int sumOfLeftLeaves(TreeNode root) {
+    return helper(root, false);
+  }
+  
+  private int helper(TreeNode node, boolean isLeft) {
+    if(node == null) return 0;
+
+    // a leave is a node without child
+    // we want to sum up all the left leaves (isLeft)
+    if(node.left == null && node.right == null && isLeft) {
+      return node.val;
+    } 
+    
+    return helper(node.left, true) + helper(node.right, false);
+  }
+}
