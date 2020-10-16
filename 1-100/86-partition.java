8bd088c6f03d17ba4be9192120a2b6b78759c07c
Lee Yoong Ti <leeyoongti@gmail.com>
2020-10-16 22:55:13 +0800

    add time and space complexity for No. 86

--- 1-100/86-partition.java
+++ 1-100/86-partition.java
@@ -1,13 +1,7 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+/*
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Partition List.
+Memory Usage: 38 MB, less than 19.10% of Java online submissions for Partition List.
+*/
 class Solution {
   public ListNode partition(ListNode head, int x) {
     var lessHead = new ListNode(0);
