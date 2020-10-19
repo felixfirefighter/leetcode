4a5e155decdac6f7de6913eeee59ecdb45e58192
Lee Yoong Ti <leeyoongti@gmail.com>
2020-10-19 23:15:14 +0800

    1007. Minimum Domino Rotations For Equal Row

---
+++ 1001-1100/1007-min-domino-rotations.java
@@ -0,0 +1,19 @@
+class Solution {
+  public int minDominoRotations(int[] A, int[] B) {
+    int[] countA = new int[7], countB = new int[7], same = new int[7];
+    int n = A.length;
+    for(var i=0;i<n;i++) {
+      countA[A[i]]++;
+      countB[B[i]]++;
+      if(A[i] == B[i]) same[A[i]]++;
+    }
+    
+    for(var i=1;i<7;i++) {
+      if(countA[i] + countB[i] - same[i] == n) {
+        return n - Math.max(countA[i], countB[i]);
+      }
+    }
+    
+    return -1;
+  }
+}
